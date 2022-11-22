3047937c32bba5d92c6c6d697e89959c29ca25bc
Dan G <dan.garton@gmail.com>
2022-11-22 12:43:47 +0200

    qt mac posix build from git

--- .github/autobuild/qt_macos_posix.sh
+++ .github/autobuild/qt_macos_posix.sh
@@ -15,26 +15,33 @@ set -eu
 # https://doc.qt.io/qt-6/qsharedmemory.html#details - see Mac section
 
 
-
 ## REQUIREMENTS (provided by Github macos build image):
 # - cmake 
 
 build_qt() {
-    # Get Qt source -
-    cd ${GITHUB_WORKSPACE}
-    MAJOR_VER=$(echo ${QT_VERSION} | cut -c -3) # get eg "6.3" when QT_VERSION=6.4.1
-    echo ">>> Downloading Qt source ..."
-    wget -q https://download.qt.io/archive/qt/${MAJOR_VER}/${QT_VERSION}/single/qt-everywhere-src-${QT_VERSION}.tar.xz
-    echo ">>> Unzipping qt-everywhere tar.xz file ..."
-    gunzip qt-everywhere-src-${QT_VERSION}.tar.xz        # uncompress the archive
-    echo ">>> Untarring qt-everywhere archive ..."
-    tar xf qt-everywhere-src-${QT_VERSION}.tar          # unpack it
+    ## Get Qt source -
+    # 1) From archives
+    # cd ${GITHUB_WORKSPACE}
+    # MAJOR_VER=$(echo ${QT_VERSION} | cut -c -3) # get eg "6.3" when QT_VERSION=6.4.1
+    # echo ">>> Downloading Qt source ..."
+    # wget -q https://download.qt.io/archive/qt/${MAJOR_VER}/${QT_VERSION}/single/qt-everywhere-src-${QT_VERSION}.tar.xz
+    # echo ">>> Unzipping qt-everywhere tar.xz file ..."
+    # gunzip qt-everywhere-src-${QT_VERSION}.tar.xz        # uncompress the archive
+    # echo ">>> Untarring qt-everywhere archive ..."
+    # tar xf qt-everywhere-src-${QT_VERSION}.tar          # unpack it
+    # cd qt-everywhere-src-${QT_VERSION}
+
+    # 2) From Git
+    cd $HOME
+    git clone git://code.qt.io/qt/qt5.git  # maybe add:  --depth 1 --shallow-submodules --no-single-branch
+    cd qt5
+    git checkout ${QT_VERSION}
+    perl init-repository --module-subset=qtbase,qtwebview,qtshadertools,qtdeclarative # get submodule source code
 
     ## Configure Qt
-    cd qt-everywhere-src-${QT_VERSION}
-        ## to build Qt with POSIX shared memory, instead of System V shared memory:
+    ## to build Qt with POSIX shared memory, instead of System V shared memory:
         # ./configure -feature-ipc_posix
-        ## for universal build:
+    ## for universal build:
         # ./configure -- -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
     # By default, Qt is configured for installation in the /usr/local/Qt-${QT_VERSION} directory,
     # but this can be changed by using the -prefix option.
